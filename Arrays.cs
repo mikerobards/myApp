3665f99a4393b7c501c63fd7af73cb2019bb8b7c
Michael Robards <mikerobards@icloud.com>
2021-08-19 16:40:49 -0400

    add more array fun!

--- Arrays.cs
+++ Arrays.cs
@@ -31,6 +31,21 @@ namespace myApp
                 Console.WriteLine(name);
             }
 
+            //another foreach
+
+            int[] inventory = { 200, 450, 700, 175, 250 };
+            int sum = 0;
+            int bin = 0;
+
+            foreach (int items in inventory)
+            {
+                sum += items;
+                bin++;
+                Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+            }
+
+            Console.WriteLine($"We have {sum} items in inventory.");
+
         }
     }
 }
